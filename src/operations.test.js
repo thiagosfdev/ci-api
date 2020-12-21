1bc8903225099642606455082f99b3029eed7f83
Thiago Figueiredo <dev01.thiagosf@gmail.com>
2020-12-20 22:24:11 -0300

    refactor: ♻️ testado sonar check com falha

--- src/operations.test.js
+++ src/operations.test.js
@@ -1,5 +1,5 @@
-const sum = require('./operations');
+const operations = require('./operations');
 
 test('adds 1 + 2 to equal 3', () => {
-    expect(sum(1, 2)).toBe(3);
+    expect(operations.sum(1, 2)).toBe(3);
 });
